de695079cb33edba7fc3a9ae9104b50c07ba620d
gillchristian <gillchristiang@gmail.com>
2017-12-18 23:22:19 +0100

    Organize code & improve joke format

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
 const trae = require('trae')
 const ora = require('ora')
 
-const traversP = ps => Promise.all(ps)
-const map = fn => xs => xs.map(fn)
-const tap = label => d => (console.log(label, d), d)
+const { traverseP, map, join, head, tail } = require('./utils')
+
 
 trae.defaults({ baseUrl: 'https://www.reddit.com' })
 
-const spinner = ora('Fetching /r/dadjokes front-page ...')
+main()
 
-console.log()
-spinner.start()
+function main() {
 
-trae.get('/r/dadjokes.json')
-  .then(r => r.data.data)
-  .then(d => d.children)
-  .then(map(child => child.data.permalink))
-  .then(map(url => url.replace(/\/$/, '')))
-  .then(map(url => `${url}.json`))
-  .then(map(trae.get))
-  .then(traversP)
-  .then(map(r => r.data[0].data))
-  .then(map(d => d.children[0].data))
-  .then(children => {
+  const spinner = ora('Fetching /r/dadjokes front-page ...')
 
-    spinner.succeed('Here are the top jokes:')
-    console.log()
-    console.log('----------------------------------------')
-    console.log()
+  console.log()
+  spinner.start()
 
-    map(child => {
-      console.log(child.title)
-      if (child.selftext) {
-        console.log('- ', child.selftext)
-      }
+  trae.get('/r/dadjokes.json')
+    .then(r => r.data.data)
+    .then(d => d.children)
+    .then(map(child => child.data.permalink))
+    .then(map(url => url.replace(/\/$/, '.json')))
+    .then(map(trae.get))
+    .then(traverseP)
+    .then(map(r => r.data[0].data))
+    .then(map(d => d.children[0].data))
+    .then(map(formatJoke))
+    .then(join('\n'))
+    .then(jokes => {
+      spinner.succeed('Here is /r/dadjokes front-page:')
       console.log()
-      console.log(child.url)
-      console.log()
-      console.log('----------------------------------------')
-      console.log()
-    })(children)
-  })
-  .catch(e => {
-    spinner.fail('There was an error:')
-    console.error(e)
-  })
+      console.log(jokes)
+    })
+    .catch(e => {
+      spinner.fail('There was an error:')
+      console.error(e)
+    })
+}
+
+function formatJoke(child) {
+  let joke = ' ' + child.title + '\n'
+
+  if (child.selftext) {
+    joke += '\n'
+
+    const lines = child.selftext.split('\n') 
+    const firstLine = head(lines)
+    const rest = tail(lines).map(line => '   ' + line).join('\n')
+
+    joke += ' - ' +  firstLine
+    if (rest) {
+      joke += rest
+    }
+  }
+
+  joke += '\n\n ' + child.url + '\n\n'
+  joke += '----------------------------------------\n'
+
+  return joke
+}
---
+++ utils.js
@@ -0,0 +1,22 @@
+// traverseP :: [Promise(a)] ->  Promise([a])
+const traverseP = ps => Promise.all(ps)
+
+// map :: (a -> b) -> [a] -> [b]
+const map = fn => xs => xs.map(fn)
+
+// join :: String -> [String] -> String
+const join = what => xs => xs.join(what)
+
+// head :: [a] -> a|undefined
+const head = xs => xs[0]
+
+// tail :: [a] -> [a]
+const tail = xs => xs.slice(1)
+
+module.exports = {
+  traverseP,
+  map,
+  join,
+  head,
+  tail
+}
